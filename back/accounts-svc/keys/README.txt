9ddb1d0287ae0e5951f26c06bc902f1a461f7dc2
rramires <ricardo@flexbr.com>
2023-02-04 15:24:24 -0300

    AccountsSvc - Add and configure jwt

---
+++ back/accounts-svc/keys/README.txt
@@ -0,0 +1,4 @@
+Use only in dev mode: https://www.csfieldguide.org.nz/en/interactives/rsa-key-generator/
+Generate RSA Key 2048 PKCS base64 and save as:
+public.key
+private.key
--- back/accounts-svc/src/auth.ts
+++ back/accounts-svc/src/auth.ts
@@ -1,4 +1,20 @@
+// imports
+import fs from 'fs';
 import bcrypt from 'bcryptjs';
+import jwt, { VerifyOptions } from 'jsonwebtoken';
+import { number } from 'joi';
+//
+// read RSA keys
+const privateKey = fs.readFileSync('./keys/private.key', 'utf-8');
+const publicKey = fs.readFileSync('./keys/public.key', 'utf-8');
+// read expire time
+const jwtExpires = parseInt(`${process.env.JWT_EXPIRES}`);
+// encrypt algorithm
+const jwtAlgorithm = "RS256";
+
+// create token type
+type Token = { accountId: number };
+
 
 /**
  * Return password hash
@@ -14,8 +30,34 @@ function hashPassword(pass: string): string{
 function comparePassword(pass: string, hash: string): boolean{
     return bcrypt.compareSync(pass, hash);
 }
+
+
+/**
+ * Return jwt token
+ */
+function sign(accountId: number){
+    const token: Token = { accountId }
+    return jwt.sign(token, privateKey, { expiresIn: jwtExpires, algorithm: jwtAlgorithm });
+}
+
+
+/**
+ * Verify jwt token
+ */
+async function verify(token: string){
+    try{
+        const decoded = await jwt.verify(token, publicKey, { algorithm: [jwtAlgorithm] } as VerifyOptions) as Token;
+        return { accountId: decoded.accountId };
+    }
+    catch(error){
+        console.log(error);
+        return null;
+    }
+}
 //
 export default{
     hashPassword,
-    comparePassword
+    comparePassword,
+    sign,
+    verify
 }
