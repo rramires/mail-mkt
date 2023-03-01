951dc096080ee9ef158223bd7310518b76f54ac9
rramires <ricardo@flexbr.com>
2023-02-28 22:32:35 -0300

    Style login form

--- front/src/pages/public/Login/index.js
+++ front/src/pages/public/Login/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 //
 import { 
+    Link
+} from 'react-router-dom';
+//
+import { 
     Container,
     Row,
     Col,
     Form,
     Button } from "react-bootstrap";
+//
+import { BoxContent, BoxForm } from './styles';
 //
 import MMLogo from '../../../assets/mmLogo.png';
 //
@@ -19,29 +25,37 @@ class Login extends React.Component{
         return (
             <Container>
                 <Row className="justify-content-md-center">
-                    <Col xs={12} md={5}>
-                        <div>
-                            <img src={MMLogo} alt="MailMarket Logo"/>
-                        </div>
-                        <h2>Login</h2>
-                        <p>Enter your credentials</p>
-                        <Form onSubmit={this.onFormSubmit}>
-                            <Form.Group controlId="emailGroup">
-                                <Form.Label>E-mail:</Form.Label>
-                                <Form.Control type="email" 
-                                              placeholder="Type your e-mail"/>
-                            </Form.Group>
-                            <Form.Group controlId="passwordGroup">
-                                <Form.Label>E-mail:</Form.Label>
-                                <Form.Control type="password" 
-                                              placeholder="Type your password"/>
-                            </Form.Group>
-                            <Form.Group className="d-grid mt-3">
-                                <Button type="submit">
-                                    Login
-                                </Button>
-                            </Form.Group>
-                        </Form>
+                    <Col xs={12} md={6}>
+                        <BoxContent>
+                            <img className="img-fluid"
+                                 src={MMLogo} 
+                                 alt="MailMarket Logo"/>
+                        </BoxContent>
+                        <BoxForm>
+                            <h2>Login</h2>
+                            <p>Enter your credentials</p>
+                            <Form onSubmit={this.onFormSubmit}>
+                                <Form.Group controlId="emailGroup">
+                                    <Form.Label>E-mail:</Form.Label>
+                                    <Form.Control type="email" 
+                                                placeholder="Type your e-mail"/>
+                                </Form.Group>
+                                <Form.Group controlId="passwordGroup">
+                                    <Form.Label>Password:</Form.Label>
+                                    <Form.Control type="password" 
+                                                placeholder="Type your password"/>
+                                </Form.Group>
+                                <Form.Group className="d-grid mt-3">
+                                    <Button type="submit">
+                                        Login
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </BoxForm>
+                        <BoxContent>
+                            <p>First Time Here?</p>
+                            <Link className="button" to="/signup">Create your account now</Link>
+                        </BoxContent>
                     </Col>
                 </Row>
             </Container>
---
+++ front/src/pages/public/Login/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BoxContent = styled.div`
+    padding: 30px 15px;
+    text-align: center;
+`;
+
+
+export const BoxForm = styled.div`
+    padding: 30px 15px;
+    border-top: solid 3px #F31300;
+    background-color: #DDDDDD;
+`;
