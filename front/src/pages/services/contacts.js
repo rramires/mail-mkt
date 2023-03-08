2d1c08c7f49b7b0bd5d50b33a9672bfdbaab7143
rramires <ricardo@flexbr.com>
2023-03-07 21:51:30 -0300

    Add contacts service

---
+++ front/src/pages/services/contacts.js
@@ -0,0 +1,51 @@
+import baseAPI from './api';
+import baseURLs from '../../config/baseURLs';
+//
+class ContactsService{
+    //
+    constructor(){
+        this.api = baseAPI(baseURLs.API_CONTACTS);
+    }
+
+
+    /**
+     * Get ALL contacts (filtered by account id)
+     */
+    async getAllContacts() {
+        const result = await this.api.get('contacts');
+        //
+        return result.data;
+    }
+
+
+    /**
+     * Get contact by id (filtered by account id too)
+     */
+    async getOneContact(contactId) {
+        const result = await this.api.get(`contacts/${contactId}`);
+        //
+        return result.data;
+    }
+
+
+    /**
+     * Create NEW Contact
+     */
+    async addContact(contactModel) {
+        const result = await this.api.post('contacts', contactModel);
+        //
+        return result;
+    }
+
+
+    /**
+     * Update contact
+     */
+    async setContact(contactModel) {
+        const result = await this.api.patch('contacts', contactModel);
+        //
+        return result;
+    }
+}
+//
+export default ContactsService
